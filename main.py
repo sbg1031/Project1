c8e1de46126ebe7689018e6c4d1e9dd71855b151
Seobeomgyu <sbg7679@gmail.com>
2023-07-06 14:51:42 +0900

    커밋 3 곱하기 함수 추가

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
     return n1-n2
 
+def gob_func(n1,n2):
+    return n1*n2
+
 ## 전역 변수부
 num1,num2,result = 100,200,0
 
@@ -14,4 +17,7 @@ print( num1, '+', num2,'=', result)
        
 result = sub_func(num1,num2)
 print( num1, '-', num2,'=', result)
+
+result = gob_func(num1,num2)
+print( num1, '*', num2,'=', result)
        
