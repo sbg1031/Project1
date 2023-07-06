03f6ed5be3a7fbf37349cf714cd525d0a099f1f7
Seobeomgyu <sbg7679@gmail.com>
2023-07-06 14:52:39 +0900

    커밋 4 나누기 함수 추가

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def sub_func(n1,n2):
 def gob_func(n1,n2):
     return n1*n2
 
+def na_func(n1,n2):
+    return n1/n2
+
 ## 전역 변수부
 num1,num2,result = 100,200,0
 
@@ -20,4 +23,8 @@ print( num1, '-', num2,'=', result)
 
 result = gob_func(num1,num2)
 print( num1, '*', num2,'=', result)
+
+result = na_func(num1,num2)
+print( num1, '/', num2,'=', result)
+       
        
